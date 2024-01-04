46110111da7bb767fd61b3b311ad1e354c417b03
jacek <jacekdusza33100@gmail.com>
2024-01-04 13:45:28 +0100

    Pobieranie dostępnych testów z serwera do drawer

--- App.tsx
+++ App.tsx
@@ -19,23 +19,22 @@ import { shuffle } from 'lodash';
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-const tests = [
-  '62032610069ef9b2616c761e',
-  '62032610069ef9b2616c761c',
-  '62032610069ef9b2616c761d',
-  '62032610069ef9b2616c761b',
-]
+const fetchServerData = async () => {
+  try {
+    const response = await fetch('https://tgryl.pl/quiz/tests');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+};
 
-const screens = [
-  'Moda na sukces', 
-  'Tranzystor bipolarny i polowy', 
-  'Wodzowie i dowódcy starożytnego Rzymu', 
-  'Zagadki matematyczne',
-]
+interface RootProps {
+  serverData: { id: string; name: string }[]; // Adjust the type based on your actual server response structure
+}
 
-const shuffledScreens = shuffle(screens);
-
-function Root() {
+function Root({ serverData }: RootProps): JSX.Element {
   return (
     <Drawer.Navigator 
       drawerContent={props => <CustomDrawer {...props} />}
@@ -54,18 +53,28 @@ function Root() {
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Results" component={ResultScreen} />
         <Drawer.Screen name="Random quiz" component={RandomTestScreen} />
-        {shuffledScreens.map((screen, index) => (
-        <Drawer.Screen key={tests[index]} name={screen} component={TestScreen} initialParams={{ customData: tests[index] }} />
-        ))}
+        {serverData.map((item) => (
+        <Drawer.Screen
+          key={item.id}
+          name={item.name}
+          component={TestScreen}
+          initialParams={{ customData: item.id }}
+        />
+      ))}
       </Drawer.Navigator>
   );
 }
 
 function App(): JSX.Element {
   const [initialScreen, setInitialScreen] = useState('Regulations');
+  const [serverData, setServerData] = useState<{ id: string; name: string }[]>([]);
  
   useEffect(() => {
-     checkRegulations();
+    checkRegulations();
+    fetchServerData().then((data) => {
+      const shuffledData: { id: string; name: string }[] = shuffle(data);
+      setServerData(shuffledData);
+    });
   }, []);
  
   const checkRegulations = async () => {
@@ -87,14 +96,20 @@ function App(): JSX.Element {
   return (
      <NavigationContainer>
        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={initialScreen}>
-         <Stack.Screen name="Root" component={Root} />
+       <Stack.Screen name="Root">
+          {(props) => <Root {...props} serverData={serverData} />}
+        </Stack.Screen>
          <Stack.Screen name="Regulations" component={RegulationsScreen} />
          <Stack.Screen name="Results" component={ResultScreen} />
          <Drawer.Screen name="Random quiz" component={TestScreen} />
-         <Drawer.Screen name="Moda na sukces" component={TestScreen} initialParams={{ customData: '62032610069ef9b2616c761e' }} />
-        <Drawer.Screen name="Tranzystor bipolarny i polowy" component={TestScreen} initialParams={{ customData: '62032610069ef9b2616c761c' }} />
-        <Drawer.Screen name="Wodzowie i dowódcy starożytnego Rzymu" component={TestScreen} initialParams={{ customData: '62032610069ef9b2616c761d' }} />
-        <Drawer.Screen name="Zagadki matematyczne" component={TestScreen} initialParams={{ customData: '62032610069ef9b2616c761b' }} />
+         {serverData.map((item) => (
+        <Drawer.Screen
+          key={item.id}
+          name={item.name}
+          component={TestScreen}
+          initialParams={{ customData: item.id }}
+        />
+      ))}
        </Stack.Navigator>
      </NavigationContainer>
   );
